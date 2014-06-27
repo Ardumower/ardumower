835d3a3e8c91616bd5dc4c29d4861645728b0a8f
greymfm@googlemail.com <greymfm@googlemail.com@3d15ab72-30b9-1f0d-4833-669fb6d4e732>
2014-06-27 09:39:45 +0000

    added missing license text

--- trunk/sender/sender.ino
+++ trunk/sender/sender.ino
@@ -1,6 +1,24 @@
 /*
-  Perimeter sender - using 'pseudonoise4_pw' signal  (for details see   http://grauonline.de/alexwww/ardumower/filter/filter.html  )
-  
+  Ardumower (www.ardumower.de)
+  Copyright (c) 2013-2014 by Alexander Grau
+  Copyright (c) 2013-2014 by Sven Gennat
+ 
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+  Perimeter sender - using 'pseudonoise4_pw' signal  (for details see   http://grauonline.de/alexwww/ardumower/filter/filter.html  )  
   for schematics, see bottom of this page:  http://www.ardumower.de/index.php/de/anleitungen/2013-11-23-19-50-19/induktion                                                                                 
                                                                                 
  */
