23ea7c22bc1d9ddb9e0ef3ee7f4cd337a47c884c
greymfm@googlemail.com <greymfm@googlemail.com@3d15ab72-30b9-1f0d-4833-669fb6d4e732>
2014-07-29 20:35:33 +0000

    added test: ultrasonic

---
+++ trunk/tests/ultrasonic/ultrasonic.ino
@@ -0,0 +1,44 @@
+
+/* ultrasonic test
+ */
+
+#include <Arduino.h>
+
+
+#ifndef __AVR__
+  #define Serial SerialUSB
+#endif
+
+
+#define pinTrigger 5
+#define pinEcho    6
+
+
+// HC-SR04 ultrasonic sensor driver
+unsigned int readHCSR04(int triggerPin, int echoPin){
+  unsigned int uS;  
+  digitalWrite(triggerPin, LOW); 
+  delayMicroseconds(2); 
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10); 
+  digitalWrite(triggerPin, LOW);
+  uS = pulseIn(echoPin, HIGH);  
+  return uS;
+}
+
+
+void setup()  {
+  Serial.begin(19200);
+  while (!Serial) ; // required if using Due native port  
+  Serial.println("START");  
+}
+
+void loop()  {
+  int x = readHCSR04(pinTrigger, pinEcho);
+  Serial.println(x);
+  delay(200);
+}
+
+
+
+
