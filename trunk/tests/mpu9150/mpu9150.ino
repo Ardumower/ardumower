83f4704aa3fe0cc3ea2561efba63dce33980b1c7
greymfm@googlemail.com <greymfm@googlemail.com@3d15ab72-30b9-1f0d-4833-669fb6d4e732>
2014-08-16 22:07:32 +0000

    added test mpu9150

--- trunk/tests/mpu9150/mpu9150.ino
+++ trunk/tests/mpu9150/mpu9150.ino
@@ -45,9 +45,10 @@ THE SOFTWARE.
 // for both classes must be in the include path of your project
 #include "I2Cdev.h"
 
-#include "MPU6050_6Axis_MotionApps20.h"
-//#include "MPU6050_9Axis_MotionApps20.h"
+//#include "MPU6050_6Axis_MotionApps20.h"
 //#include "MPU6050.h" // not necessary if using MotionApps include file
+#include "MPU6050_9Axis_MotionApps41.h"
+
 
 // Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
 // is used in I2Cdev.h
