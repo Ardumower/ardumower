97a6eea25bbbd2e6016900d2c26ddbf72eec3dd9
greymfm@googlemail.com <greymfm@googlemail.com@3d15ab72-30b9-1f0d-4833-669fb6d4e732>
2014-08-21 12:59:44 +0000

    test: updated files

--- trunk/tests/perimeterV2/perimeter.cpp
+++ trunk/tests/perimeterV2/perimeter.cpp
@@ -29,15 +29,13 @@
 int signalsize = 0;
 int8_t matchSignal[100];
 
-// http://grauonline.de/alexwww/ardumower/filter/filter.html
-// "pulse3" signal                   
+
 void Perimeter::gensignal(){
-  //Console.println("gensignal");
   // http://grauonline.de/alexwww/ardumower/filter/filter.html    
   // "pseudonoise4_pw" signal
-  //int8_t pncode[] = { 1,1,-1,-1,-1,1,-1,-1,1,1,-1,1,-1,1,1,-1 };
+  int8_t pncode[] = { 1,1,-1,-1,-1,1,-1,-1,1,1,-1,1,-1,1,1,-1 };
   // "pseudonoise5_pw" signal
-  int8_t pncode[] = { 1,1,1,-1,-1,-1,1,1,-1,1,1,1,-1,1,-1,1,-1,-1,-1,-1,1,-1,-1,1,-1,1,1,-1,-1,1,1,-1 };
+  //int8_t pncode[] = { 1,1,1,-1,-1,-1,1,1,-1,1,1,1,-1,1,-1,1,-1,-1,-1,-1,1,-1,-1,1,-1,1,1,-1,-1,1,1,-1 };
   int step=0;
   byte width = 1;
   int8_t state = -1;
@@ -73,16 +71,14 @@ Perimeter::Perimeter(){
   mag[0] = mag[1] = 0;
   smoothMag = 0;
   signalCounter = 0;  
-  filterMinSmooth = 0;
-  filterMaxSmooth = 0;
-  //signalsize = sizeof matchSignal;
+  lastInsideTime = 0;
   gensignal();  
 }
 
 void Perimeter::setPins(byte idx0Pin, byte idx1Pin){
   idxPin[0] = idx0Pin;
   idxPin[1] = idx1Pin;  
-  ADCMan.setCapture(idx0Pin, signalsize*2, true); 
+  ADCMan.setCapture(idx0Pin, signalsize*5, true); 
   
   Console.print("matchSignal size=");
   Console.println(signalsize);  
@@ -143,20 +139,23 @@ void Perimeter::matchedFilter(byte idx){
     }
     signalAvg = ((double)signalAvg) / ((double)(sampleCount));
   }
-  for (int i=0; i < sampleCount; i++) {
+  /*for (int i=0; i < sampleCount; i++) {
     if (samples[i]>0) samples[i]=1;
       else samples[i] = -1;
-  }
-  // magnitude for tracking (fast but inaccurate)  
-  mag[idx] = convFilter(matchSignal, signalsize, samples, sampleCount-signalsize);      
+  }*/
+  // magnitude for tracking (fast but inaccurate)    
+  mag[idx] = convFilter(matchSignal, signalsize, samples, sampleCount-signalsize);        
   smoothMag = 0.99 * smoothMag + 0.01 * ((float)mag[idx]);
 
   // perimeter inside/outside detection
   if (mag[idx] > 0){
-    signalCounter = min(signalCounter + 1, 3);    
+    signalCounter = min(signalCounter + 1, 6);    
   } else {
-    signalCounter = max(signalCounter - 1, -3);    
+    signalCounter = max(signalCounter - 1, -6);    
   }
+  if (signalCounter < 0){
+    lastInsideTime = millis();
+  } 
     
   ADCMan.restart(idxPin[idx]);    
   callCounter++;
@@ -178,8 +177,9 @@ boolean Perimeter::isInside(){
   return (signalCounter < 0);  
 }
 
+
 boolean Perimeter::signalTimedOut(){
-  return (smoothMag > 0);
+  return (millis() - lastInsideTime > 8000);
 }
 
 
