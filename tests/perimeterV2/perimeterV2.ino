debba3e528d7322125c71613f0d17b410c6d8ed5
greymfm@googlemail.com <greymfm@googlemail.com@3d15ab72-30b9-1f0d-4833-669fb6d4e732>
2015-01-23 11:20:48 +0000

    perimeter: corrected filter quality

--- tests/perimeterV2/perimeterV2.ino
+++ tests/perimeterV2/perimeterV2.ino
@@ -17,15 +17,17 @@
 #include "adcman.h"
 #include "perimeter.h"
 
-
-#define pinPerimeterRight A4       // perimeter
-#define pinPerimeterLeft A5
+#define pinPerimeterLeft A5        // perimeter pin
+#define pinPerimeterRight A4       // leave open
 #define pinLED 13
+
+// #define USE_DEVELOPER_TEST 1
 
 Perimeter perimeter;
 unsigned long nextTime = 0;
 int counter = 0;
 boolean inside = true;
+int mode = 0;
 
 
 void setup()  {  
@@ -37,25 +39,13 @@ void setup()  {
   Console.println("START");        
   
   ADCMan.init();
-  perimeter.setPins(pinPerimeterLeft, pinPerimeterRight);        
-  ADCMan.calibrate();
+  perimeter.setPins(pinPerimeterLeft, pinPerimeterRight);          
   
   perimeter.speedTest();
 }
 
-void loop()  {     
-      
-  ADCMan.run();  
-  
-  if (millis() >= nextTime){
-    nextTime = millis() + 50;
-    if (perimeter.isInside() != inside){
-      inside = perimeter.isInside();
-      counter++;      
-    }
-    if (perimeter.isInside()) digitalWrite(pinLED, HIGH);    
-      else digitalWrite(pinLED, LOW);    
-    
+
+void printConsole(){
     Console.print("cnt ");
     Console.print(counter);
     Console.print("\t");    
@@ -76,7 +66,7 @@ void loop()  {
     Console.print((int)perimeter.getSmoothMagnitude());                
     Console.print("\t");    
     Console.print("qty ");        
-    Console.print((int)perimeter.getFilterQuality()*100);
+    Console.print((int)(perimeter.getFilterQuality()*100.0));
     Console.print("\t");
     Console.print("\t");
     Console.print("in ");
@@ -87,7 +77,58 @@ void loop()  {
     Console.print("\t");
     Console.print("adc ");
     Console.print((int)(ADCMan.getCapturedChannels()));            
-    Console.println();
+    Console.println();   
+}
+
+
+// https://code.google.com/p/serialchart/
+void printSerialChart(){
+  if (!ADCMan.isCaptureComplete(pinPerimeterLeft)) return;
+  int8_t *samples = ADCMan.getCapture(pinPerimeterLeft);      
+  int sz = ADCMan.getCaptureSize(pinPerimeterLeft);      
+  int mag = perimeter.getMagnitude(0);              
+  int qty = perimeter.getFilterQuality()*100.0;      
+  for (int i=0; i < sz; i++){
+    for (int w=0; w < 10; w++){
+      if (i==0){
+        Console.print(qty);
+      } else Console.print("0");
+      Console.print(",");
+      Console.println(samples[i]);
+    }
+  }
+  delay(5000);
+}
+
+void loop()  {     
+      
+  ADCMan.run();  
+  
+  if (Console.available() > 0){
+    char ch = (char)Console.read();      
+    if (ch == 'v') mode = !mode;
+    if (ch == 'c') {
+      Console.println("calibrating ADC (power off sender for this!)...");
+      Console.flush();
+      delay(5000);
+      ADCMan.calibrate();
+    }
+  }
+  
+  if (millis() >= nextTime){
+    nextTime = millis() + 50;
+    if (perimeter.isInside() != inside){
+      inside = perimeter.isInside();
+      counter++;      
+    }
+    if (perimeter.isInside()) digitalWrite(pinLED, HIGH);    
+      else digitalWrite(pinLED, LOW);    
+
+    if (mode == 0){
+      printConsole();
+    } else {
+      printSerialChart();
+    }
   }
 }
 
