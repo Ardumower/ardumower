b8e0b48b4823744c1aaa877fa6adc0e4566bd660
greymfm@googlemail.com <greymfm@googlemail.com@3d15ab72-30b9-1f0d-4833-669fb6d4e732>
2015-02-01 09:51:09 +0000

    removed simbad

--- trunk/sim/world.cpp
+++ trunk/sim/world.cpp
@@ -5,6 +5,7 @@ World::World(){
   imgBfield = Mat(SIZE_Y, SIZE_X, CV_8UC3, Scalar(0,0,0));
   imgWorld = Mat(SIZE_Y, SIZE_X, CV_8UC3, Scalar(0,0,0));
 
+  // perimeter lines coordinates
   std::vector<point_t> list;
   list.push_back( (point_t) {30, 35 } );
   list.push_back( (point_t) {400, 40 } );
@@ -12,26 +13,29 @@ World::World(){
   list.push_back( (point_t) {210, 250 } );
   list.push_back( (point_t) {50, 310 } );
 
+  // compute magnetic field (compute distance to perimeter lines)
   int x1 = list[list.size()-1].x;
   int y1 = list[list.size()-1].y;
+  // for each perimeter line
   for (int i=0; i < list.size(); i++){
     int x2 = list[i].x;
     int y2 = list[i].y;
     int dx = (x2-x1);
     int dy = (y2-y1);
-    int len=(sqrt( dx*dx + dy*dy ));
-    float phi = atan2(dy,dx);
+    int len=(sqrt( dx*dx + dy*dy )); // line length
+    float phi = atan2(dy,dx); // line angle
+    // compute magnetic field for points (x,y) around perimeter line
     for (int y=-200; y < 200; y++){
       for (int x=-100; x < len*2+100-1; x++){
         int px= x1 + cos(phi)*x/2 - sin(phi)*y;
         int py= y1 + sin(phi)*x/2 + cos(phi)*y;
-        int xend = max(0, min(len, x/2));
-        int cx = x1 + cos(phi)*xend;
-        int cy = y1 + sin(phi)*xend;
+        int xend = max(0, min(len, x/2)); // restrict to line ends
+        int cx = x1 + cos(phi)*xend;  // cx on line
+        int cy = y1 + sin(phi)*xend;  // cy on line
         if ((py >= 0) && (py < SIZE_Y)
            && (px >=0) && (px < SIZE_X)) {
-          float r = max(0.000001, sqrt( (cx-px)*(cx-px) + (cy-py)*(cy-py) ) );
-          float b=1000.0/(2.0*M_PI*r);
+          float r = max(0.000001, sqrt( (cx-px)*(cx-px) + (cy-py)*(cy-py) ) ); // distance to line
+          float b=1000.0/(2.0*M_PI*r); // field strength
           if ((y<=0) || (bfield[py][px] < 0)){
             b=b*-1.0;
             bfield[py][px] =  min(bfield[py][px], b);
@@ -43,6 +47,7 @@ World::World(){
     y1=y2;
   }
 
+  // draw magnetic field onto image
   for (int y=0; y < SIZE_Y; y++){
     for (int x=0; x < SIZE_X; x++) {
       float b=30 + 30*sqrt( abs(bfield[y][x]));
@@ -64,9 +69,9 @@ World::World(){
 }
 
 float World::getBfield(int x, int y){
-    if ((x >= 0) && (x < SIZE_X) && (y >= 0) && (y < SIZE_Y)){
+  if ((x >= 0) && (x < SIZE_X) && (y >= 0) && (y < SIZE_Y)){
         return bfield[y][x];
-    } else return 0;
+  } else return 0;
 }
 
 void World::draw(){
