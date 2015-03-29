5975b987e98f521e724a60ef8d31c3377fbf626b
greymfm@googlemail.com <greymfm@googlemail.com@3d15ab72-30b9-1f0d-4833-669fb6d4e732>
2015-03-29 18:56:39 +0000

    added pfodapp test

--- trunk/tests/pfodapp/pfodapp.ino
+++ trunk/tests/pfodapp/pfodapp.ino
@@ -13,10 +13,20 @@
 
 int values[30] = {0};
 int counter = 0;
+float sliderValue = 42;
+
 unsigned long nextCounterTime = 0;
 String pfodCmd;
 boolean pfodCmdComplete = false;
 
+
+float stringToFloat(String &s){
+  float v;
+  char tmp[20];  
+  s.toCharArray(tmp, sizeof(tmp));
+  v = atof(tmp);    
+  return v;
+}
 
 int stringToInt(String &s){
   int v;
@@ -32,15 +42,50 @@ void sendYesNo(int value){
     else Bluetooth.print("NO");
 }
 
+// NOTE: pfodApp rev57 changed slider protocol:  displayValue = (sliderValue + offset) * scale
+void sendSlider(String cmd, String title, float value, String unit, double scale, float maxvalue, float minvalue){
+  Bluetooth.print("|"); 
+  Bluetooth.print(cmd);  
+  Bluetooth.print("~");    
+  Bluetooth.print(title);
+  Bluetooth.print(" `");      
+  Bluetooth.print(((int)(value/scale)));
+  Bluetooth.print("`");  
+  Bluetooth.print(((int)(maxvalue/scale)));
+  Bluetooth.print("`");
+  Bluetooth.print(((int)(minvalue/scale)));
+  Bluetooth.print("~ ~");
+  if (scale == 10) Bluetooth.print("10");
+    else if (scale == 1) Bluetooth.print("1");
+    else if (scale == 0.1) Bluetooth.print("0.1");
+    else if (scale == 0.01) Bluetooth.print("0.01");
+    else if (scale == 0.001) Bluetooth.print("0.001");
+    else if (scale == 0.0001) Bluetooth.print("0.0001");
+}
+
+
+void processSlider(String result, float &value, double scale){
+  int idx = result.indexOf('`');
+  String s = result.substring(idx + 1);      
+  float v = stringToFloat(s);
+  value = v * scale;  
+}
 
 void sendMainMenu(boolean update){
   if (update) Bluetooth.print("{:"); else {
     Bluetooth.print(F("{.Test`1000"));      
   }
-  Bluetooth.print("|c00~Counter is ");    
+  Bluetooth.print("|m0_0~Counter is ");    
   Bluetooth.print(counter);  
+  
+  sendSlider("m1_0", F("Slider1 "), sliderValue, "", 1, 1000, 0);
+  sendSlider("m1_1", F("Slider2 "), sliderValue, "", 1, 1000, 0);    
+  sendSlider("m1_2", F("Slider3 "), sliderValue, "", 1, 1000, 0);  
+  sendSlider("m1_3", F("Slider4 "), sliderValue, "", 1, 1000, 0);    
+  sendSlider("m1_4", F("Slider5 "), sliderValue, "", 1, 1000, 0);      
+  
   for (int i=0; i < 30; i++){
-    Bluetooth.print("|m");
+    Bluetooth.print("|m2_");
     Bluetooth.print(i);
     Bluetooth.print("~Item ");
     Bluetooth.print(i);
@@ -51,12 +96,16 @@ void sendMainMenu(boolean update){
 }
 
 void processMainMenu(String pfodCmd){
-  int idx = pfodCmd.indexOf('m');
-  String s = pfodCmd.substring(idx + 1);      
-  idx = stringToInt(s);
-  //Console.print("idx=");
-  //Console.println(idx);
-  values[idx] =! values[idx];
+  if (pfodCmd.startsWith("m1")){
+    processSlider(pfodCmd, sliderValue, 1);    
+  } else if (pfodCmd.startsWith("m2")) {  
+    int idx = pfodCmd.indexOf('_');
+    String s = pfodCmd.substring(idx + 1);      
+    idx = stringToInt(s);
+    //Console.print("idx=");
+    //Console.println(idx);
+    values[idx] =! values[idx];
+  } 
   sendMainMenu(true);  
 }
 
