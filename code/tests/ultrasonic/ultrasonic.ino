c56ad0b6c3812032daafc325a2fc185fcb2a4349
AlexanderG <alexandergrau@gmx.de>
2017-04-22 22:32:52 +0200

    ultrasonic: 3 sensors

--- code/tests/ultrasonic/ultrasonic.ino
+++ code/tests/ultrasonic/ultrasonic.ino
@@ -8,8 +8,14 @@
 #include "RunningMedian.h"
 
 
-#define pinTrigger 24
-#define pinEcho    22
+#define pinTrigger1 24
+#define pinEcho1    22
+
+#define pinTrigger2 36
+#define pinEcho2    34
+
+#define pinTrigger3 44
+#define pinEcho3    42
 
 
 // ultrasonic sensor max echo time (WARNING: do not set too high, it consumes CPU time!)
@@ -18,15 +24,21 @@
 #define NO_ECHO 0
 
 
-RunningMedian<unsigned int,50> sonarMeasurements;
+RunningMedian<unsigned int,20> sonar1Measurements;
+RunningMedian<unsigned int,20> sonar2Measurements;
+RunningMedian<unsigned int,20> sonar3Measurements;
+volatile byte echoPin = pinEcho1;
 volatile unsigned long startTime = 0;
 volatile unsigned long echoTime = 0;
 volatile unsigned long echoDuration = 0;
 unsigned long timeoutTime = 0;
+unsigned long nextSonarTime = 0;
+int idx = 0;
 
 
 // HC-SR04 ultrasonic sensor driver (2cm - 400cm)
-void startHCSR04(int triggerPin, int echoPin){
+void startHCSR04(int triggerPin, int aechoPin){
+  echoPin = aechoPin;
   unsigned int uS;          
   digitalWrite(triggerPin, LOW); 
   delayMicroseconds(2); 
@@ -42,7 +54,7 @@ void startHCSR04(int triggerPin, int echoPin){
 }
 
 void echoSignal(){
-  if (digitalRead(pinEcho) == HIGH) {    
+  if (digitalRead(echoPin) == HIGH) {    
     echoTime = 0;
     startTime = micros();           
   } else {    
@@ -53,36 +65,58 @@ void echoSignal(){
 
 
 void setup()  {
-  pinMode(pinTrigger , OUTPUT);
-  pinMode(pinEcho , INPUT);  
+  pinMode(pinTrigger1 , OUTPUT);
+  pinMode(pinTrigger2 , OUTPUT);
+  pinMode(pinTrigger3 , OUTPUT);
+  pinMode(pinEcho1 , INPUT);  
+  pinMode(pinEcho2 , INPUT);  
+  pinMode(pinEcho3 , INPUT);  
 
-  attachInterrupt(pinEcho, echoSignal, CHANGE);
+  attachInterrupt(pinEcho1, echoSignal, CHANGE);
+  attachInterrupt(pinEcho2, echoSignal, CHANGE);
+  attachInterrupt(pinEcho3, echoSignal, CHANGE);
 
-  Serial.begin(115200);  
-  Serial.println("START");  
-  Serial.println("raw,avg,median,triggered");      
+  Serial.begin(115200);     
+  Serial.println("START");
 }
 
 void loop()  {
   int triggered = 0;
   float avg;
-  unsigned int median;
-  unsigned long raw;
-  if (millis() > timeoutTime){    
-    startHCSR04(pinTrigger, pinEcho);        
-    timeoutTime = millis() + 10;
+  unsigned int median1 = 0;
+  unsigned int median2 = 0;
+  unsigned int median3 = 0;
+  unsigned long raw;  
+  if (millis() > timeoutTime){            
+    if (millis() > nextSonarTime){        
+      idx = (idx + 1) % 3;
+      nextSonarTime = millis() + 200;
+    }
+    if (idx == 0) startHCSR04(pinTrigger1, pinEcho1);        
+      else if (idx == 1) startHCSR04(pinTrigger2, pinEcho2);        
+      else startHCSR04(pinTrigger3, pinEcho3);        
+    timeoutTime = millis() + 5;    
   }
   if (echoDuration != 0) {            
-    raw = echoDuration;
-    sonarMeasurements.add(raw);        
-    sonarMeasurements.getAverage(avg);      
-    sonarMeasurements.getMedian(median);   
-    Serial.print(avg);    
-    Serial.print(",");    
-    Serial.print(median); 
-    Serial.print(",");                             
-    Serial.print(raw);    
-    Serial.print(",");                             
+    raw = echoDuration;    
+    if (raw > 4000) raw = 4000;
+    if (idx == 0) sonar1Measurements.add(raw);        
+      else if (idx == 1) sonar2Measurements.add(raw);        
+      else sonar3Measurements.add(raw);        
+    sonar1Measurements.getAverage(avg);      
+    sonar1Measurements.getMedian(median1);   
+    sonar2Measurements.getMedian(median2);   
+    sonar3Measurements.getMedian(median3);   
+    /*Serial.print(avg);    
+    Serial.print(",");    */
+    Serial.print(median1); 
+    Serial.print(",");  
+    Serial.print(median2); 
+    Serial.print(",");  
+    Serial.print(median3); 
+    Serial.print(",");     
+    //Serial.print(raw);    
+    //Serial.print(",");                             
     Serial.print(5000);    
     Serial.print(",");                             
     Serial.print(0);    
