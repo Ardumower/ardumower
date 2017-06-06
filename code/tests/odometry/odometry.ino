2c683a96e26dda3d68085d2ccd80195c93e31a6b
AlexanderG <alexandergrau@gmx.de>
2017-06-06 21:38:57 +0200

    odometry test: pcb 1.3 and mini works

--- code/tests/odometry/odometry.ino
+++ code/tests/odometry/odometry.ino
@@ -15,32 +15,19 @@
 int odometryLeft ;   // left wheel counter
 int odometryRight ;  // right wheel counter
 
+int lastOdometryLeft = 0 ;   
+int lastOdometryRight = 0;  
 
 volatile long oldOdoPins_A = 0;
 volatile long oldOdoPins_B = 0;
   
-ISR(PCINT2_vect)
-{
-    const long actPins_A = REG_PIOA_PDSR;             // read PIO A
-    const long actPins_B = REG_PIOB_PDSR;                               // read PIO B
-    const long setPins_A = (oldOdoPins_A ^ actPins_A);
-    const long setPins_B = (oldOdoPins_B ^ actPins_B);
-        
-    //Right
-    if (setPins_A & 0b00000000000000000000000000000010)     // pin right has changed 
-    {      
-      odometryRight++;      
-      oldOdoPins_A = actPins_A;
-    }
-    
-    //Left
-    if (setPins_B & 0b00000000000000001000000000000000)           // pin left has changed
-    {            
-      odometryLeft++;                 
-      oldOdoPins_B = actPins_B;
-    }     
+void OdometryRightInt(){
+  odometryRight++;      
 }
 
+void OdometryLeftInt(){
+  odometryLeft++;                          
+}
 
 
 void setup()  {
@@ -50,24 +37,27 @@ void setup()  {
   Serial.println("START");  
   
   // odometry
-  //pinMode(pinOdometryLeft, INPUT_PULLUP);  
-  //pinMode(pinOdometryRight, INPUT_PULLUP);
-  pinMode(pinOdometryLeft, INPUT);  
-  pinMode(pinOdometryRight, INPUT);  
+  pinMode(pinOdometryLeft, INPUT_PULLUP);  
+  pinMode(pinOdometryRight, INPUT_PULLUP);
+  //pinMode(pinOdometryLeft, INPUT);  
+  //pinMode(pinOdometryRight, INPUT);  
   
-  attachInterrupt(pinOdometryLeft, PCINT2_vect, CHANGE);    
-  attachInterrupt(pinOdometryRight, PCINT2_vect, CHANGE);  
-  //PinMan.setDebounce(pinOdometryLeft, 100);  // reject spikes shorter than usecs on pin
-  //PinMan.setDebounce(pinOdometryRight, 100);  // reject spikes shorter than usecs on pin         
+  attachInterrupt(pinOdometryLeft, OdometryLeftInt, RISING);    
+  attachInterrupt(pinOdometryRight, OdometryRightInt, RISING);  
+  PinMan.setDebounce(pinOdometryLeft, 100);  // reject spikes shorter than usecs on pin
+  PinMan.setDebounce(pinOdometryRight, 100);  // reject spikes shorter than usecs on pin         
 }
 
 void loop()  {  
-  Serial.print(odometryLeft);    
-  Serial.print(",");    
-  Serial.print(odometryRight);    
-  Serial.println();    
-  
-  delay(500);
+  if ( (odometryLeft != lastOdometryLeft) || (odometryRight != lastOdometryRight) ){    
+    Serial.print(odometryLeft);    
+    Serial.print(",");    
+    Serial.print(odometryRight);    
+    Serial.println();       
+    lastOdometryLeft = odometryLeft;
+    lastOdometryRight = odometryRight;    
+  }  
+  delay(50);
 }
 
 
