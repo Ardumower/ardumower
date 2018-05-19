de449f24d758be4c53ef6981d46858b2373234ca
AlexanderG <alexandergrau@gmx.de>
2018-05-19 15:42:30 +0200

    FIXME: this is a odometryTheta hack to get correct results?

--- code/ardumower/motor.h
+++ code/ardumower/motor.h
@@ -27,8 +27,7 @@ void Robot::calcOdometry(){
   double left_cm = ((double)ticksLeft) / ((double)odometryTicksPerCm);
   double right_cm = ((double)ticksRight) / ((double)odometryTicksPerCm);  
   double avg_cm  = (left_cm + right_cm) / 2.0;
-  double wheel_theta = (left_cm - right_cm) / ((double)odometryWheelBaseCm);
-  odometryTheta += wheel_theta; 
+  odometryTheta = scalePI(odometryTheta + wheel_theta); 
   
 	// calculate RPM 
   motorLeftRpmCurr  = double ((( ((double)ticksLeft) / ((double)odometryTicksPerRevolution)) / ((double)(millis() - lastMotorRpmTime))) * 60000.0); 
